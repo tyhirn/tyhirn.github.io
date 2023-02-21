c0a0a8eedd13db7441462858e50e1a96f31eb6d9
Tyler Hirn <76914455+tyhirn@users.noreply.github.com>
2023-02-21 12:17:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // JavaScript source code
+// Navigation toggle
+const navToggle = document.querySelector('.nav__toggle');
+const navMenu = document.querySelector('.nav__menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('nav__menu--active');
+});
+
+// Smooth scrolling
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const target = document.querySelector(link.getAttribute('href'));
+    const offsetTop = target.offsetTop;
+
+    window.scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  });
+});
