3c0684a842e17f753c065e2aa2eb8eff037037d2
Tyler <tylerrhirn@gmail.com>
2023-03-19 19:09:32 -0500

    ALPHA v1 done!

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const links = document.querySelectorAll('a[href^="#"]');
 links.forEach(link => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
     const target = document.querySelector(link.getAttribute('href'));
     const offsetTop = target.offsetTop;
 
@@ -23,3 +23,21 @@ links.forEach(link => {
     });
   });
 });
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("jobs__StyledTabPanel");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+openTab(event, 'tab1');
+
+
